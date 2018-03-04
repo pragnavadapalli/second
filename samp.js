8dd6df1d2d417490b1dab192c6787e66c7f3d685
pragnavadapalli <pragna3098@gmail.com>
2018-03-04 12:02:33 +0530

    commiting second time

--- samp.js
+++ samp.js
@@ -13,7 +13,11 @@ getfile("sample.json",function(text){
   let data=JSON.parse(text);
   console.log(data);
   basicinfo(data.basics);
+  education(data.education);
 });
+
+var main=document.querySelector('.flex-parent')
+
 function basicinfo(basic){
   var profile=document.getElementById("basics");
   var name=document.createElement("h1");
@@ -28,6 +32,46 @@ function basicinfo(basic){
     var photo=document.createElement("img");
     photo.src=basic.photo;
     profile.appendChild(photo);
-
-
     }
+
+
+
+
+  var right=document.createElement("div");
+  right.classList.add("content-child");
+  main.appendChild(right);
+  //appendChild ie.. right div to main
+  //education div
+  function education(education){
+  var e1=document.createElement("div");
+  e1.classList.add("edu");
+  right.appendChild(e1);
+  //appending dive1 to right
+  var h1=document.createElement("h1");
+  h1.setAttribute("id","heading");
+  h1.textContent="Education Details";
+  e1.appendChild(h1);
+  //appending h1 to e1 div
+  // function eduinfo(education){
+    for(i in education){
+        //creation of course
+      var h2=document.createElement("h2");
+      h2.classList.add("edu1");
+      h2.innerHTML=education[i].course;
+      //creation of College
+      h1.appendChild(h2);
+      var h3=document.createElement("h3");
+      h3.classList.add("edu2");
+      h3.textContent=education[i].college;
+      h2.appendChild(h3);
+     var ul=document.createElement("ul");
+     ul.classList.add("edu3");
+     h3.appendChild(ul);
+        for(j in education){
+           var li=document.createElement("li");
+           li.textContent=education[i].data[j];
+           ul.appendChild(li);
+        }
+        // ul.appendChild(li);
+    }
+  }
